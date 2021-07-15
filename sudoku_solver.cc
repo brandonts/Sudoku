89ab33f9fa27bc131fd34195be02319cb9b74e48
Brandon Shaw <brandonts@hotmail.com>
2021-07-14 18:26:58 -0600

    refactored sudoku_solver files to combine them

--- sudoku_solver.cc
+++ sudoku_solver.cc
@@ -5,12 +5,15 @@
  *      Author: Brandon Shaw
  */
 
-#include "sudoku_solver.h"
 #include "sudoku_logic.h"
 #include "sudoku_puzzle.h"
 
 #include <iostream>
+
 using namespace std;
+
+void Display(SudokuPuzzle &p1,int row,int column);
+void Display(SudokuPuzzle &p1);
 
 int main()
 {
@@ -67,3 +70,43 @@ int main()
 
 	return 0;
 }
+
+void Display(SudokuPuzzle &p1)
+ {
+	for(int i=0;i<9;i++)
+	{
+		if(i==0 || i==3 || i==6) cout<<"\n-------------------------\n";
+		else cout<<"\n";
+		cout << "| ";
+		for(int j=0;j<9;j++)
+		{
+		        cout << p1.Read_(i,j);
+		        if(j==2 || j==5 || j==8) cout<<" | ";
+		        else cout<<" ";
+		}
+	}
+	cout<<"\n-------------------------\n";
+}
+ 
+void Display(SudokuPuzzle &p1,int row,int column)
+{
+	for(int i=0;i<9;i++)
+	{
+		if(i==0 || i==3 || i==6) cout<<"\n-------------------------\n";
+		else cout<<"\n";
+		for(int j=0;j<9;j++)
+		{
+			if(j==0) cout<<"|";
+			if(i==row && j==column) cout<<"("<<p1.Read_(i,j)<<")";
+			else if(i==row && j==column+1) cout<<p1.Read_(i,j);
+			else cout<<" "<<p1.Read_(i,j);
+			if(j==2 || j==5 || j==8)
+			{
+				if(i==row && j==column) cout<<"| ";
+				else cout<<" |";
+			}
+                 }
+         }
+         cout<<"\n-------------------------\n";
+}
+
